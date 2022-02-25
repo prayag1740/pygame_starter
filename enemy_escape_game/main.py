9f2edbc753e2bb87893f818b650c1c7aca3568fb
Prayag Bhatia <prayag.bhatia2015@vit.ac.in>
2022-02-25 11:47:49 +0530

    ADD : sounds and frame rate changes

--- enemy_escape_game/main.py
+++ enemy_escape_game/main.py
@@ -14,12 +14,21 @@ from pygame.locals import (
     QUIT
 )
 
+#setup for sounds
+pygame.mixer.init()
+
+pygame.mixer.music.load('enemy_escape_game/assets/game_sound.mp3')
+pygame.mixer.music.play(loops=-1)
+
 pygame.init() #initializing pygame
 
 SCREEN_HEIGHT = 800
 SCREEN_WIDTH = 800
 
 screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDTH))
+
+#setup the clock for decent framerate
+clock = pygame.time.Clock()
 
 
 #game imp 
@@ -30,6 +39,24 @@ screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDTH))
 4)maintains the speed of the game
 """
 #Sprites Group -- group of sprite objects
+
+class Cloud(pygame.sprite.Sprite):
+
+    def __init__(self):
+        super(Cloud, self).__init__()
+        self.surf = pygame.image.load('enemy_escape_game/assets/cloud.jpeg')
+        self.surf.set_colorkey((0,0,0), RLEACCEL)
+        self.surf = pygame.transform.scale(self.surf, (40,40))
+        self.rect = self.surf.get_rect(
+            center=(random.randint(SCREEN_WIDTH+20, SCREEN_HEIGHT+100),
+            (random.randint(0, SCREEN_HEIGHT))
+            )
+        )
+
+    def update(self):
+        self.rect.move_ip(-5,0)
+        if self.rect.right < 0:
+            self.kill()
 
 
 #sprite is a 2D representation of any object on the screen
@@ -78,7 +105,7 @@ class Enemy(pygame.sprite.Sprite):
                 random.randint(0, SCREEN_HEIGHT),              
             )
         )
-        self.speed = random.randint(5, 120)
+        self.speed = random.randint(5, 10)
 
     def update(self):
         self.rect.move_ip(-self.speed,0) #moves the enemy to the left side of screen
@@ -95,11 +122,13 @@ player = Player()
 # all_sprites -- used for rendering
 enemies = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
+clouds = pygame.sprite.Group()
 all_sprites.add(player)
 
 ADDENEMY = pygame.USEREVENT + 1  #defining a unique event for pygame
 pygame.time.set_timer(ADDENEMY, 250) #adds event in event queue after every 250 ms
-
+ADDCLOUD = pygame.USEREVENT + 2
+pygame.time.set_timer(ADDCLOUD, 500)
 start_time = time.time()
 
 while running:
@@ -116,8 +145,13 @@ while running:
             enemies.add(new_enemy)
             all_sprites.add(new_enemy)
 
+        elif event.type == ADDCLOUD:
+            new_cloud = Cloud()
+            clouds.add(new_cloud)
+            all_sprites.add(new_cloud)
+
     #white background for screen
-    screen.fill((0, 0, 0))
+    screen.fill((135, 206, 250))
 
     #creates a surface (rectangular object)and params are length and width
     surf = pygame.Surface((50, 50))
@@ -137,6 +171,7 @@ while running:
     player.update(pressed_keys)
 
     enemies.update()
+    clouds.update()
 
     for entity in all_sprites:
         screen.blit(entity.surf, entity.rect)
@@ -150,6 +185,8 @@ while running:
         running = False 
 
     pygame.display.flip() #displays actual
+
+    clock.tick(30) # 30 frames per second
 
 pygame.quit()
             
